5284f908d1d894119f2cb02090dfaad698eda77b
SEljaafari <78881640+SEljaafari@users.noreply.github.com>
2023-12-03 17:13:36 +0100

    feat(Consulation) Affichage et tri des resultats selon le rapport prix/Nuit

--- src/main/java/fr/samyseb/tripadvisor/services/OffreService.java
+++ src/main/java/fr/samyseb/tripadvisor/services/OffreService.java
@@ -7,9 +7,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -19,7 +18,7 @@ public class OffreService {
 
     private final AgenceRepository agenceRepository;
 
-    public List<Offre> create(LocalDate debut, LocalDate fin, Float prixMin, Float prixMax) {
+    public List<Offre> create(LocalDate debut, LocalDate fin, Float prixMin, Float prixMax, Integer etoilesMin) {
         return StreamSupport.stream(agenceRepository.findAll().spliterator(), true)
                 .map(agence -> WebClient.create()
                         .get()
@@ -31,12 +30,20 @@ public class OffreService {
                                 .queryParam("fin", fin)
                                 .queryParam("prixMin", prixMin)
                                 .queryParam("prixMax", prixMax)
+                                .queryParam("etoilesMin", etoilesMin)
                                 .build())
                         .retrieve()
                         .bodyToMono(Offre[].class)
                         .block())
                 .filter(Objects::nonNull)
                 .flatMap(Arrays::stream)
+                .map(offre -> {
+                    long nbNuits = ChronoUnit.DAYS.between(debut, fin);
+                    float prixParNuit = offre.prixSejour() / nbNuits;
+                    return new AbstractMap.SimpleEntry<>(offre, prixParNuit);
+                })
+                .sorted(Comparator.comparing(Map.Entry::getValue))
+                .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
     }
 
